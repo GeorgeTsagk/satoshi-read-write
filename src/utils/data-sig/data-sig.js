6ef47b6f3c425aea95476d32589c72d3ca8325f4
George Tsagkarelis <georgets98@gmail.com>
2022-06-04 18:28:00 +0300

    Add DataSig struct to transmitted TLVs

---
+++ src/utils/data-sig/data-sig.js
@@ -0,0 +1,89 @@
+const { signer } = require('../../lnd-rpc/signer')
+var protobuf = require("protobufjs")
+
+const generateDataSig = (version, data, destinationAddress, senderAddress) => {
+    return new Promise(function (resolve, reject) {
+        const destBuff = Buffer.from(destinationAddress, "hex");
+        var senderBuff
+        if (senderAddress !== undefined
+            && typeof (senderAddress) == 'string') {
+            senderBuff = Buffer.from(senderAddress, "hex");
+        }
+
+        const destDataBuffer = Buffer.concat([destBuff, data])
+
+        let request = {
+            msg: destDataBuffer,
+            double_hash: false,
+            compact_sig: true,
+            key_loc: {
+                key_family: 6,
+                key_index: 0
+            }
+        };
+        signer.signMessage(request, function (err, response) {
+            if (err) {
+                reject(err)
+            }
+            if (response) {
+                let dataSig = {
+                    version: version,
+                    sig: response.signature
+                }
+                if (senderAddress !== undefined
+                    && typeof senderAddress == 'string') {
+                    dataSig['senderPK'] = senderAddress
+                }
+                resolve(dataSig)
+            }
+        });
+    })
+}
+
+const encodeDataSig = (dataSig) => {
+    return new Promise(function (resolve, reject) {
+        protobuf.load(__dirname + '/data-sig.proto', function (err, root) {
+            if (err)
+                reject(err);
+
+            var DataSigMessage = root.lookupType("datasig.DataSig");
+
+            var errMsg = DataSigMessage.verify(dataSig);
+            if (errMsg)
+                reject(Error(errMsg));
+
+            var message = DataSigMessage.create(dataSig);
+
+            var buffer = DataSigMessage.encode(message).finish();
+
+            resolve(buffer)
+        })
+    })
+
+}
+
+const decodeDataSig = (dataSigBuffer) => {
+    return new Promise(function (resolve, reject) {
+        protobuf.load(__dirname + '/data-sig.proto', function (err, root) {
+            if (err)
+                reject(err);
+
+            var DataSigMessage = root.lookupType("datasig.DataSig");
+
+            var message = DataSigMessage.decode(dataSigBuffer);
+            var object = DataSigMessage.toObject(message, {
+                longs: String,
+                enums: String,
+            });
+
+            resolve(object)
+        });
+    })
+}
+
+
+module.exports = {
+    encodeDataSig,
+    decodeDataSig,
+    generateDataSig
+}
--- src/write-sats.js
+++ src/write-sats.js
@@ -3,20 +3,24 @@ const { lightning } = require('./lnd-rpc/lightning');
 const { randomBytes, createHash } = require('crypto')
 const configLoader = require('./config/config-loader')
 
+const { generateDataSig, encodeDataSig } = require('./utils/data-sig/data-sig')
+
 const config = configLoader.getConfig()
 
-const sendPayment = (address, data) => {
+const sendPayment = (address, data, dataSigBuffer) => {
     const preimage = randomBytes(32)
     const hash = createHash('sha256').update(preimage).digest()
 
     const destBuff = Buffer.from(address, "hex");
 
     const dataKey = Number(config.data_struct.tlv_key)
+    const sigKey = Number(config.data_sig.tlv_key)
 
     let records = {}
     records[5482373484] = preimage
     // records[dataKey] = Buffer.from(data, 'utf-8')
-    records[dataKey] = Buffer.from(data, 'utf-8')
+    records[dataKey] = data
+    records[sigKey] = dataSigBuffer
 
     let request = {
         dest: destBuff,
@@ -44,7 +48,13 @@ const sendPayment = (address, data) => {
 }
 
 const sendDataToAddress = (address, data) => {
-    sendPayment(address, data)
+    generateDataSig(1, data, address, undefined)
+    .then((res) => encodeDataSig(res))
+    .then((sigBuf) => sendPayment(
+        address,
+        data,
+        sigBuf
+    ))
 }
 
 module.exports = {
