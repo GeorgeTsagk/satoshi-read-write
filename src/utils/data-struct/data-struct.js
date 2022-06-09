fdc8c4f25d61d207f2f06109431684093cfe3a3a
George Tsagkarelis <georgets98@gmail.com>
2022-06-09 13:14:37 +0300

    Add transmitted data size limit

--- src/utils/data-struct/data-struct.js
+++ src/utils/data-struct/data-struct.js
@@ -7,6 +7,12 @@ var protobuf = require("protobufjs")
 const DATA_STRUCT_VERSION = 1
 
 const dataToDataStructArray = (dataBuffer) => {
+    if(dataBuffer.length > config.data_struct.max_data_size) {
+        console.error(`You are trying to transmit a file bigger than the configured max size.\n
+                        Transmitting data bigger than a few MB can consume big amounts of sats.\n
+                        If you want to execute this operation please change the maximum allowed size in the configuration file.\n`)
+        return
+    }
     const totalSize = dataBuffer.length
     const fragmentationId = Math.floor(Math.random() * Math.pow(10, 9));
     const fragmentSize = config.data_struct.max_fragment_bytes
