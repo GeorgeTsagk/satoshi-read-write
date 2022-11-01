ddf65d8cc98d73f76a7da1db7a23ae1e1084567b
George Tsagkarelis <george.tsagkarelis@gmail.com>
2022-11-01 17:28:35 +0200

    Add stablediffusion post request

--- src/api-handlers.js
+++ src/api-handlers.js
@@ -13,11 +13,11 @@ const apiHandler = (args, senderAddress, amt) => {
 }
 
 handlers['stablediffusion'] = {
-    guard: function (args, replyAddress, amt) {
+    guard: (args, replyAddress, amt) => {
         if (amt >= this.price) return true;
     },
-    price: 1,
-    method: function (args, replyAddress, amt) {
+    price: 100,
+    method: async (args, replyAddress, amt) => {
         args.shift()
 
         if (args.length == 0) {
@@ -30,7 +30,45 @@ handlers['stablediffusion'] = {
 
         // **START**
 
-        //TODO kon: call your rtx4090 in here
+        const params = {
+            "enable_hr": False,
+            "denoising_strength": 0,
+            "firstphase_width": 0,
+            "firstphase_height": 0,
+            "prompt": "example prompt",
+            "styles": [],
+            "seed": -1,
+            "subseed": -1,
+            "subseed_strength": 0,
+            "seed_resize_from_h": -1,
+            "seed_resize_from_w": -1,
+            "batch_size": 1,
+            "n_iter": 1,
+            "steps": 3,
+            "cfg_scale": 7,
+            "width": 64,
+            "height": 64,
+            "restore_faces": False,
+            "tiling": False,
+            "negative_prompt": "",
+            "eta": 0,
+            "s_churn": 0,
+            "s_tmax": 0,
+            "s_tmin": 0,
+            "s_noise": 1,
+            "sampler_index": "Euler a"
+        }
+
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(params)
+        }
+
+        fetch(url, options)
+            .then( response => response.json())
+            .then( response => {
+                console.log("Response from stablediffusion:", response)
+            })
 
         //TODO kon: store your image somewhere and provide the absolute path below
         const filepath = "REPLACEME"
